1401f94374927c8df4f42feda49114e30d99f754
marioomendezz <mario.mendezc@educem.net>
2024-10-18 16:45:03 +0200

    Moviment jugador

--- Player.gd
+++ Player.gd
@@ -15,3 +15,22 @@ func _ready():
 #	pass
 export var speed = 400 # A quina velocitat es mourà el jugador (píxels/seg).
 var screen_size # Mida de la finestra de joc.
+
+func _process(delta):
+	var velocity = Vector2.ZERO # Vector de moviment del jugador.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
