dbb16d03c9c65d2879639985cfd9f78bc601b271
marioomendezz <mario.mendezc@educem.net>
2024-10-18 16:43:05 +0200

    Assignació de tecles

--- Player.gd
+++ Player.gd
@@ -8,12 +8,10 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	screen_size = get_viewport_rect().size
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
 export var speed = 400 # A quina velocitat es mourà el jugador (píxels/seg).
 var screen_size # Mida de la finestra de joc.
