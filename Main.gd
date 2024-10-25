0a2b21ff2bb20fe5284515328276483e5a7d0415
marioomendezz <mario.mendezc@educem.net>
2024-10-25 16:33:25 +0200

    Joc jugable V1

--- Main.gd
+++ Main.gd
@@ -10,11 +10,14 @@ func _ready():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
-	
+	$HUD.show_game_over()
+
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func _on_MobTimer_timeout():
 	# Crea una nova instància de l'escena Mob.
@@ -41,3 +44,8 @@ func _on_StartTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
+
+func update_score(score):
+	$ScoreLabel.text = str(score)
+
